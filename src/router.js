582b2b3ac8582e08cb3f6daafe33d9ea89ec2285
Dmitrij <d.kamkov@splinestudio.ua>
2023-04-06 12:14:43 +0300

    routing for deploy fix

--- src/router.js
+++ src/router.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import MainPage from './pages/MainPage'
 import FavoritePage from './pages/FavoritePage'
 
 export default createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes: [
         { path: '/', name: 'Main', component: MainPage },
         { path: '/favorite', name: 'Favorite', component: FavoritePage }
